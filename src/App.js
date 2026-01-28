33e96e703d5de6656f9b2a539c45d99bb22db8bb
suleyman <150763893+suleyman0931@users.noreply.github.com>
2026-01-28 14:26:32 +0300

    Change developer photo source from

--- src/App.js
+++ src/App.js
@@ -1802,7 +1802,7 @@ function AboutUsModal({ onClose, translations }) {
               <div className="developer-card">
                 <div className="developer-photo">
                   <img 
-                    src="/tesnim.png" 
+                    src="/tesnim2.png" 
                     alt="Tesnim"
                     onError={(e) => {
                       e.target.style.display = 'none';
